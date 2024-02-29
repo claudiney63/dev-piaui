b44c28dc8b06af9e462df79b3353bde8136f186f
claudiney63 <claudineyryan75@gmail.com>
2024-02-29 05:21:17 -0300

    Add homepage content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,37 @@
 import NavBarComponent from "../../components/NavBarComponent";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function Home() {
   return (
     <>
-    <NavBarComponent></NavBarComponent>
+      <NavBarComponent></NavBarComponent>
+
+      {/* Inicio */}
+      <Container
+        fluid
+        id="content"
+        style={{ marginTop: "20px" }}
+        className="p-4"
+      >
+        <Row className="align-items-center justify-content-center">
+          <Col lg={6}>
+            <div className="text-center">
+              <h2>Bem-vindo ao Dev Piauí</h2>
+              <p>A sua empresa de desenvolvimento web, simples e direta.</p>
+              <Button variant="dark">Fale conosco</Button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="text-center mt-4">
+              <img
+                src="https://www.efront.com/efront-c-assets/cache-1640080016000/images/media-bin/web/global/banner/limited-partners.webp"
+                alt="Imagem"
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
